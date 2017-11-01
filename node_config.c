0b5f9e952e4756c646a17ec84e0143b5eff84ddb
Laurent TANNOUS <laurent.tannous@free.fr>
2017-11-01 23:25:11 +0100

    modifying cmd args testing and adding config request in gc

--- node_config.c
+++ node_config.c
@@ -13,7 +13,7 @@ typedef enum {
 
 void nrf_setup() {
     char status;
-    char ConfigAddr[] = {'A', 'D', 'D', 'R', '0'};
+    char ConfigAddr[] = {'A', 'D', 'D', 'R', '0'}; // Address for tx/rx
     nrf_init();
     nrf_clear_rx_dr(&status);
     nrf_clear_max_rt(&status);
@@ -21,15 +21,24 @@ void nrf_setup() {
     nrf_flush_tx(&status);
     nrf_flush_rx(&status);
     nrf_set_rx_address(P0, ConfigAddr, &status);
+    nrf_enable_dynamic_payload(P0, &status);
     nrf_set_tx_address(ConfigAddr, &status);
 	nrf_set_power_mode(POWER_ON, &status);
     nrf_set_data_rate(DR1MBPS, &status);
+    nrf_set_auto_retransmit_count(5, &status);
+	nrf_set_auto_retransmit_delay(15, &status);
     printf("NRF initialisation done\n");
 }
 
 void getConfig() {
-    // Send the getconfig request
-        
+    char status;
+    const char * getconfstr = "config_request";
+    nrf_set_mode_ptx(&status);
+    
+    // Send the getconfig request    
+    nrf_write_tx_payload(getconfstr, strlen(getconfstr), &status);
+    nrf_tx_payload();
+    sleep(1);
     // Waiting for getconfig answer
 
     // Display the node remote configuration
@@ -47,10 +56,10 @@ int main(int argc, char *argv[]) {
     char status;
     int  Mode;
 
-    if ((argc == 2) && strcmp(argv[1],"gc")) {
+    if ((argc == 2) && (strcmp(argv[1],"gc") == 0)) {
         // Get config mode
         Mode = getConfigMode;
-    } else if ((argc == 6) && strcmp(argv[1],"sc")) {
+    } else if ((argc == 6) && (strcmp(argv[1],"sc") == 0)) {
         // Set config mode
         Mode = setConfigMode;
     } else {
@@ -61,13 +70,14 @@ int main(int argc, char *argv[]) {
 	nrf_setup();
     
     if (Mode == getConfigMode) {
+        printf("Get configuration requested\n");
         getConfig();
     } else if (Mode == setConfigMode) {
         char nodeAddr[5];
         char gwAddr[5];
         int rfPower;
         int rfChannel;
-        
+        printf("Set configuration requested\n");
         // Extract parameters info
         setConfig(nodeAddr, gwAddr, rfPower, rfChannel);
     }
